d2ca7e116a57e3765e0484772ce19f81951f72f6
Piettro Rodrigues <piettroenrico@hotmail.com>
2025-07-09 16:38:12 +0200

    Class5

--- school/views.py
+++ school/views.py
@@ -1,28 +1,38 @@
 from school.models import Student, Course, Enrollment
 from school.serializers import StudentSerializer, CourseSerializer, EnrollmentSerializer, ListEnrollmentSerializerPerStudent, ListEnrollmentSerializerPerCourse
 from rest_framework import viewsets, generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 class StudentViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing student instances.
     """
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAdminUser]
+
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CourseViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing course instances.
     """
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing course instances.
     """
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAdminUser]
+
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
 
@@ -30,6 +40,9 @@ class ListEnrollmentPerStudent(generics.ListAPIView):
     """
     A viewset for listing enrollments per student.
     """
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = ListEnrollmentSerializerPerStudent
 
     def get_queryset(self):
@@ -40,6 +53,9 @@ class ListEnrollmentPerCourse(generics.ListAPIView):
     """
     A viewset for listing enrollments per course.
     """
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = ListEnrollmentSerializerPerCourse
 
     def get_queryset(self):
