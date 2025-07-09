9a3f43427e300851f29359c8e37f4fe359574785
Piettro Rodrigues <piettroenrico@hotmail.com>
2025-07-09 15:22:57 +0200

    Class2

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date_birth', 'email', 'cpf', 'phone_number')
+    list_display_links = ('id', 'name',)
+    list_per_page = 20
+    search_fields = ('name', 'email', 'cpf', 'phone_number',)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description')
+    list_display_links = ('id', 'code',)
+    list_per_page = 20
+    search_fields = ('code',)
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
--- school/models.py
+++ school/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    date_birth = models.DateField()
+    email = models.EmailField(unique=True, blank=True, max_length=30)
+    cpf = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=14, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Course(models.Model):
+    level_choices = [
+        ('B', 'Beginner'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=10, unique=True)    
+    description = models.TextField(max_length=100, blank=False)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    level = models.CharField(max_length=50, blank=False, null=False, default='B' ,choices=level_choices)
+
+    def __str__(self):
+        return self.code
+    
